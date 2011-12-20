7e704298fcd0c757aa1735b1fc9f85dc263e828f
Gregory Lou <gregory.lou@gmail.com>
2011-12-19 21:48:24 -0500

    initial check in of stripe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stripe::Application.config.secret_token = 'b66b75d10149663c14edd4c50f9c2f346c80195fad75c6fd36efc3fed05ddf34ab71c39a89aa7c71c6a3247bdb2d968e923fe063ca6f8cf72148cac99aa15e18'
